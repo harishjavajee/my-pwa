d4470067521e42505530ece76b5743c13ccd6547
harishjavajee <59080816+harishjavajee@users.noreply.github.com>
2025-04-23 22:13:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 if ('serviceWorker' in navigator) {
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stop Chrome from showing the prompt automatically
+  e.preventDefault();
+
+  // Save the event so we can trigger it later
+  deferredPrompt = e;
+
+  // Show your custom "Install" button or UI
+  document.getElementById('installBtn').style.display = 'block';
+});
+
+document.getElementById('installBtn').addEventListener('click', () => {
+  if (deferredPrompt) {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install');
+      } else {
+        console.log('User dismissed the install');
+      }
+
+      deferredPrompt = null; // Reset it
+    });
+  }
+});
+  
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('Service Worker registered:', reg))
